6b93749abd95095961aa52ae285842dee166f989
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2018-02-19 18:37:04 +0530

    Serializer updated.
    [1] using string buffers instead of files.

--- Assignment_1/Problem 2/serializer.c
+++ Assignment_1/Problem 2/serializer.c
@@ -5,93 +5,139 @@
 #include <string.h>
 #include <stdbool.h>
 
-void serialize_trie(TrieNode* root, FILE** fp1, FILE** fp2, int child_index){
+void MAY_REALLOC(char** fp, int* fp_count, int* fp_size){
+    if((*fp_count) >= (*fp_size) - 1000){
+        int size = (*fp_size);
+        *fp = (char*) realloc(*fp, 2 * size * sizeof(char));
+        memset((*fp) + (*fp_size), '\0', *fp_size);
+        (*fp_size) = 2 * size;
+        // *(*fp + (*fp_size) -1) = '\0';
+    }
+}
+
+void serialize_trie(TrieNode* root, char** fp1, char** fp2, int child_index, int* fp1_count, int* fp2_count, int* fp1_size, int* fp2_size){
     if(root == NULL){
     	return;
     }
 
     if(child_index == -1){
- 		fprintf(*fp1, "%c ", '$');
+        *(*fp1 + *fp1_count) = '$';
+        (*fp1_count) ++;
+        MAY_REALLOC(fp1, fp1_count, fp1_size);
+ 		// fprintf(*fp1, "%c ", '$');
  	}else{
  		if(root -> end){
-    		fprintf(*fp1, "%c ", index_to_char(child_index));
-    		fprintf(*fp1, "%c ", '(');
-    		serialize_list(root -> list, fp2);
+    		// fprintf(*fp1, "%c ", index_to_char(child_index));
+    		// fprintf(*fp1, "%c ", '(');
+            *(*fp1 + *fp1_count) = index_to_char(child_index);
+            (*fp1_count) ++;
+            MAY_REALLOC(fp1, fp1_count, fp1_size);
+            *(*fp1 + *fp1_count) = '(';
+            (*fp1_count) ++;
+            MAY_REALLOC(fp1, fp1_count, fp1_size);
+    		serialize_list(root -> list, fp2, fp2_count, fp2_size);
     	}else{
-    		fprintf(*fp1, "%c ", index_to_char(child_index));
+    		// fprintf(*fp1, "%c ", index_to_char(child_index));
+            *(*fp1 + *fp1_count) = index_to_char(child_index);
+            (*fp1_count) ++;
+            MAY_REALLOC(fp1, fp1_count, fp1_size);
     	}
  	}
 
     int i;
  	for(i = 0; i < ALPHABET_SIZE; i++){
-       	serialize_trie(root -> children[i], fp1, fp2, i);
+       	serialize_trie(root -> children[i], fp1, fp2, i, fp1_count, fp2_count, fp1_size, fp2_size);
     }
  
-    fprintf(*fp1, "%c ", '|');
+    // fprintf(*fp1, "%c ", '|');
+    *(*fp1 + *fp1_count) = '|';
+    (*fp1_count) ++;
+    MAY_REALLOC(fp1, fp1_count, fp1_size);
 }
 
 
-TrieNode* deserialize_trie(TrieNode* root, FILE** fp1, FILE** fp2){
+TrieNode* deserialize_trie(TrieNode* root, char** fp1, char** fp2, int* fp1_count, int* fp2_count){
     char val;
 	int garbage;
 
     while(1){
-	    garbage = fscanf(*fp1, "%c ", &val);
+	    // garbage = fscanf(*fp1, "%c ", &val);
+        val = *(*fp1 + *fp1_count);
+        (*fp1_count) ++;
     	// printf("%c ", val);
 
-    	if(feof(*fp1) || garbage == 0){
-    		break;
-    	}
+    	// if(feof(*fp1) || garbage == 0){
+    	// 	break;
+    	// }
 
-	    if(garbage != 0){
-	    	if(val == '|'){
-				return root;
-	    	}else if(val == '$'){
-	    		root = get_clus_Node();
-	    		root = deserialize_trie(root, fp1, fp2);
-	    		break;
-	    	}else if(val == '('){
-		    	root -> end = 1;
-		    	root -> list = deserialize_list(fp2);
-	    		root = deserialize_trie(root, fp1, fp2);
-	    		break;
-	    	}else{
-		    	int index = CHAR_TO_INDEX(val);
-	    		root -> children[index] = get_clus_Node();
-	    		root -> children[index] = deserialize_trie(root -> children[index], fp1, fp2);
-	    	}
+	    // if(garbage != 0){
+	    if(val == '|'){
+			return root;
+	    }else if(val == '$'){
+	    	root = get_clus_Node();
+	    	root = deserialize_trie(root, fp1, fp2, fp1_count, fp2_count);
+	    	break;
+	    }else if(val == '('){
+		   	root -> end = 1;
+		   	root -> list = deserialize_list(fp2, fp2_count);
+	    	root = deserialize_trie(root, fp1, fp2, fp1_count, fp2_count);
+	    	break;
+	    }else{
+		   	int index = CHAR_TO_INDEX(val);
+	    	root -> children[index] = get_clus_Node();
+	    	root -> children[index] = deserialize_trie(root -> children[index], fp1, fp2, fp1_count, fp2_count);
 	    }
+	    // }
 	}
 
 	return root;
 }
 
-void serialize_list(List* list, FILE** fp){
+void serialize_list(List* list, char** fp, int* fp_count, int* fp_size){
 	Node* temp = list -> head;
+    // printf("%d\n", list  -> size);
+    char* store = (char*) malloc(1000 * sizeof(char));
 	while(temp != NULL){
-		fprintf(*fp, "%d %s\t", temp -> ele -> frequency, temp -> ele -> doc_name);
+		sprintf(store, "%d\t%s\t", temp -> ele -> frequency, temp -> ele -> doc_name);
+        strcat(*fp, store);
+        while(*(*fp + *fp_count) != '\0'){
+            (*fp_count) ++;
+        }
+        if((*fp_count) >= (*fp_size) - 1000){
+            MAY_REALLOC(fp, fp_count, fp_size);
+        }
 		temp = temp -> next;
 	}
-	fprintf(*fp, "-1\n");
+	sprintf(store, "-1\n");
+    strcat(*fp, store);
 }
 
-List* deserialize_list(FILE** fp){
+List* deserialize_list(char** fp, int* fp_count){
 	List* list = create_list();
-	int garbage, frequency;
-	char* temp_str = (char*) malloc(25 * sizeof(char));
+	int garbage;
+    unsigned int frequency;
+	char* temp_str = (char*) malloc(1000 * sizeof(char));
 
 	while(1){
 		Element* data = (Element*) malloc(sizeof(Element));
-		data -> doc_name = (char*) malloc(25 * sizeof(char));
-		garbage = fscanf(*fp, "%d", &frequency);
+		data -> doc_name = (char*) malloc(1000 * sizeof(char));
+		garbage = sscanf((*fp + *fp_count), "%d", &frequency);
+        while(*(*fp + *fp_count) != '\t' && *(*fp + *fp_count) != '\n'){
+            (*fp_count) ++;
+        }
+        (*fp_count) ++;
 		if(frequency == -1){
+            // (*fp_count) += 2;
 			break;
 		}
-		garbage = fscanf(*fp, "%s", temp_str);
-
-		if(feof(*fp) || garbage == 0){
-			break;
-		}
+		garbage = sscanf((*fp + *fp_count), "%s", temp_str);
+        while(*(*fp + *fp_count) != '\t' && *(*fp + *fp_count) != '\n'){
+            (*fp_count) ++;
+        }
+        (*fp_count) ++;
+		// if(feof(*fp) || garbage == 0){
+		// 	break;
+		// }
 
 		data -> frequency = frequency;
 		strcpy(data -> doc_name, temp_str);
